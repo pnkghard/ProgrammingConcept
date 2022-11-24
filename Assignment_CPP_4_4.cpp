293cd94edf06b281c837124fbc133734aab48b32
pnkghard <85216539+pnkghard@users.noreply.github.com>
2022-11-24 12:56:47 +0530

    Create Assignment_CPP_4_4.cpp

---
+++ Assignment_CPP_4_4.cpp
@@ -0,0 +1,12 @@
+/*
+Assignment - Lab 4
+    Print the address of the variables defined.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int *v;
+    cout << "Address of variable is " << v << endl;
+}
