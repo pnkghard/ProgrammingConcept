838e9612685b96269715d870e047ca81458ba50e
pnkghard <85216539+pnkghard@users.noreply.github.com>
2022-11-24 12:56:43 +0530

    Create Assignment_CPP_4_2.cpp

---
+++ Assignment_CPP_4_2.cpp
@@ -0,0 +1,14 @@
+/*
+Assignment - Lab 4
+    Write a program to count number of charter in string.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cout << "Enter string : ";
+    cin >> s;
+    cout << "count : " << s.length() << endl;
+}
