fe48d6000c6cca581fad163dd8cdf507797b2f30
pnkghard <85216539+pnkghard@users.noreply.github.com>
2022-11-24 14:42:28 +0530

    Create Assignment_CPP_5_1.cpp

---
+++ Assignment_CPP_5_1.cpp
@@ -0,0 +1,46 @@
+/*
+Assignment - Lab                                                                      
+    Create demo applications to illustrate different types of inheritance.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+class Student {
+    protected:  string name;
+                int id;
+
+    public: Student();
+            Student(string name, int id) { // constructor of student class
+                this->name = name;
+                this->id = id;
+            }
+
+            void print() {
+                cout << "Student Name : " << this->name << endl;
+                cout << "Student id   : " << this->id << endl;
+            }
+};
+
+class Member: public Student {
+    long long number;
+    string course;
+    Student s;
+    public: Member(string name, int id, long long number, string course) {
+                Student(name, id); // inheretention
+                this->number = number;
+                this->course = course;
+            }
+            // function overloading
+            void print(){
+                s.print();
+                cout << "Number of Student : "  << this->number << endl;
+                cout << "Course of Student : "  << this->course << endl;
+            }
+};
+
+int main(){
+    Member mr("Pankaj", 15, 8788131068, "HPCAP");
+    mr.print();
+}
